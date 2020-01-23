ea55b1670f90cdd4d54ab2ce5b420d87e0b1d6bf
Saurabh Kushwah <saurabhkushwah@outlook.com>
2020-01-23 20:58:55 +0530

    Added specfiy audio file name using command line argument Added any file type extension of audio file

--- transcribe.py
+++ transcribe.py
@@ -1,18 +1,40 @@
 import speech_recognition as sr
 from os import path
 from pydub import AudioSegment
+import sys
 
-# convert mp3 file to wav
-sound = AudioSegment.from_mp3("transcript.mp3")
+# Check for filename in argument
+if len(sys.argv) <= 1:
+    print("Audio File Not provided!!\n Exiting...")
+    sys.exit(1)
 
+# Check for filename and fileExtension
+
+filename = sys.argv[1]
+fileExtension = filename.split('.')[-1]
+
+print("File Name : ", filename)
+print("File Type : ", fileExtension)
+
+print("Audio Conversion to wav format")
+# Convert the audio file into wav format
 sound = AudioSegment.from_file(filename, fileExtension)
 sound.export("transcript.wav", format="wav")
 
+print("Conversion Complete  !")
+print("Converting into text :")
+
 # transcribe audio file
 AUDIO_FILE = "transcript.wav"
+
+textForm = None
 
 # use the audio file as the audio source
 r = sr.Recognizer()
 with sr.AudioFile(AUDIO_FILE) as source:
     audio = r.record(source)  # read the entire audio file
-    print("Transcription: " + r.recognize_google(audio))
+    textForm = r.recognize_google(audio)
+    # print("Transcription: " + r.recognize_google(audio))
+
+print("Converted !!")
+print(textForm)
